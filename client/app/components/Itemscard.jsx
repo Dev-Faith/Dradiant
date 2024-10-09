a8a2b92bfbf68b2dc5869a9a087a999102591a71
Faith Adebayo <63503378+Dev-Faith@users.noreply.github.com>
2024-10-09 15:47:16 +0100

    started designing the itemscard component

--- client/app/components/Itemscard.jsx
+++ client/app/components/Itemscard.jsx
@@ -6,13 +6,13 @@ export default function Itemscard({name, price, image, desc, liked}){
     return (
         <div className="border-[1px] border-[#7B7768] rounded-[20px] w-[472px] h-[566px] cursor flex flex-col items-center px-[17px] py-[24px] gap-[51px]">
            <div className="image&Liked flex w-full justify-between">
-             <Image src = {image} width="267" height="338" className="relative left-[76px]"/>
+             <Image src = {image} width="267" height="250" className="relative left-[76px] "/>
              <div className="like relative  bg-[#fff] rounded-full size-[55px] border-[1px] border-[#7B7768] flex items-center justify-center"><IoIosHeartEmpty className="size-[39px] text-[#7B7768]"/></div>
            </div>
-           <div className="flex flex-col gap-[29px]">
+           <div className="flex flex-col gap-[25px]">
              <div className="desc&name flex flex-col gap-[13px]">
                  <p className="text-[32px] text-[#6A5F11]">{name.toUpperCase()}</p>
-                 <p className="text-[28px] text-[#1D1C13]">{desc}</p>
+                 <p className="text-[28px]/[36px] text-[#1D1C13]">{desc}</p>
              </div>
              <div className="details&Price flex justify-between items-baseline">
                  <p className="underline text-[#426651] text-[32px]">Details</p>
