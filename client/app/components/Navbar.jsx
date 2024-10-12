12718151d74a9703b9e641545a7897aaf2b1e312
Faith Adebayo <63503378+Dev-Faith@users.noreply.github.com>
2024-10-12 23:06:49 +0100

    done with the signin and signup pages

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -79,54 +79,70 @@ const Layout = () => {
   }
 
   return (
-    <div className="xl:px-[123px] px-[11px] lg:px-[50px] md:px-[50px] py-[24px] flex justify-between items-center border-b-[1px] w-[100%] border-[#7B7768]">
-      <div className="text-[24px] xl:hidden items-start">
-        <IoSearch />
-      </div>
-      <div className="left xl:flex gap-[32px] text-[16px] hidden">
-        {navLinks.map((link) =>
-          link.position === "left" ? (
-            <Link href={link.url}>
-              <m.p
-                whileHover={{ scale: 1.5 }}
-                whileTap={{ scale: 1 }}
-                key={link.name}
-                className={`cursor-pointer hover:underline hover:text-[#2A4E3A] ${isActive(link.url) ? " underline text-[#2A4E3A]": ""}`}
-              >
-                {link.name}
-              </m.p>
+    <>
+      {currentPath !== "/pages/signin" && currentPath !== "/pages/signup" && (
+        <div className="xl:px-[123px] px-[11px] lg:px-[50px] md:px-[50px] py-[24px] flex justify-between items-center border-b-[1px] w-[100%] border-[#7B7768]">
+          <div className="text-[24px] xl:hidden items-start">
+            <IoSearch />
+          </div>
+          <div className="left xl:flex gap-[32px] text-[16px] hidden">
+            {navLinks.map((link) =>
+              link.position === "left" ? (
+                <Link href={link.url}>
+                  <m.p
+                    whileHover={{ scale: 1.5 }}
+                    whileTap={{ scale: 1 }}
+                    key={link.name}
+                    className={`cursor-pointer hover:underline hover:text-[#2A4E3A] ${
+                      isActive(link.url) ? " underline text-[#2A4E3A]" : ""
+                    }`}
+                  >
+                    {link.name}
+                  </m.p>
+                </Link>
+              ) : null
+            )}
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Link href="/">
+              <p className="logo text-[22px] text-center cursor-pointer w-full">
+                DRADIANTBAGS
+              </p>
             </Link>
-          ) : null
-        )}
-      </div>
-      <div className="flex items-center justify-center w-full">
-        <Link href="/" ><p className="logo text-[22px] text-center cursor-pointer w-full">DRADIANTBAGS</p></Link>
-      </div>
-      <div className="text-[24px] flex items-center w-[86px] justify-between xl:hidden">
-        <Link href="https://bit.ly/ShopfromDradiant">
-          <TiShoppingCart />
-        </Link>
-        <RiMenu3Line className="size-[24px]" onClick={toggleMenu} />
-        <AnimatePresence>{isMenuOpen && <MenuSheet closeMenu={toggleMenu} isOpen={isMenuOpen} />}</AnimatePresence>
-      </div>
-      <div className="right xl:flex gap-[32px] hidden">
-        {navLinks.map((link) =>
-          link.position === "right" ? (
-            <Link href={link.url} className="relative">
-               {notificationButton(link.url)}
-              <m.p
-                whileHover={{ scale: 1.5 }}
-                whileTap={{ scale: 1 }}
-                key={link.name}
-                className={`cursor-pointer hover:underline hover:text-[#2A4E3A] ${isActive(link.url) ? " underline text-[#2A4E3A]": ""}`}
-              >
-                {link.name}
-              </m.p>
+          </div>
+          <div className="text-[24px] flex items-center w-[86px] justify-between xl:hidden">
+            <Link href="https://bit.ly/ShopfromDradiant">
+              <TiShoppingCart />
             </Link>
-          ) : null
-        )}
-      </div>
-    </div>
+            <RiMenu3Line className="size-[24px]" onClick={toggleMenu} />
+            <AnimatePresence>
+              {isMenuOpen && (
+                <MenuSheet closeMenu={toggleMenu} isOpen={isMenuOpen} />
+              )}
+            </AnimatePresence>
+          </div>
+          <div className="right xl:flex gap-[32px] hidden">
+            {navLinks.map((link) =>
+              link.position === "right" ? (
+                <Link href={link.url} className="relative">
+                  {notificationButton(link.url)}
+                  <m.p
+                    whileHover={{ scale: 1.5 }}
+                    whileTap={{ scale: 1 }}
+                    key={link.name}
+                    className={`cursor-pointer hover:underline hover:text-[#2A4E3A] ${
+                      isActive(link.url) ? " underline text-[#2A4E3A]" : ""
+                    }`}
+                  >
+                    {link.name}
+                  </m.p>
+                </Link>
+              ) : null
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default Layout;
---
+++ client/app/pages/signin/page.jsx
@@ -0,0 +1,45 @@
+const page = () => {
+  return (
+    <div className="px-[125px] pt-[64px] flex flex-col items-center gap-[73px]">
+      <div className="welcome flex flex-col gap-[16px] justify-center">
+        <p className="text-[36px] text-center">Welcome back to</p>
+        <p className="text-[36px] logo text-center">DRADIANTBAGS</p>
+      </div>
+      <form className="flex flex-col w-[657px] h-[610px] rounded-[30px] bg-[#E8E2D4] px-[135px] py-[64px] gap-[30px] items-center">
+        <p className="signin text-[32px] self-start">Sign In:</p>
+        <div className="email self-start flex flex-col gap-[10px] w-full">
+          <label htmlFor="" className="self-start">
+            Email
+          </label>
+          <input
+            type="email"
+            className="self-start bg-[#E8E2D4] border-[1px] border-[#6A5F11] px-[16px] py-[8px] w-full rounded-[8px] outline-none"
+            placeholder="e.g Olubimo@gmail.com"
+          />
+        </div>
+        <div className="password self-start flex flex-col gap-[10px] w-full">
+          <label htmlFor="" className="self-start">
+            password
+          </label>
+          <input
+            type="password"
+            className="self-start bg-[#E8E2D4] border-[1px] border-[#6A5F11] px-[16px] py-[8px] w-full rounded-[8px] outline-none"
+            placeholder="***"
+          />
+        </div>
+        <button
+          type="submit"
+          className="self-start bg-[#6A5F11] text-[16px] text-[#fff] p-[16px] rounded-[8px] w-full"
+        >
+          Sign In
+        </button>
+        <div className="signup flex items-center gap-[8px]">
+          <p className="text-[16px]">Got no account?</p>
+          <p className="text-[#426651] underline text-[16px]">Sign up</p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default page;
