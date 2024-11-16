42ef68be5e9b5408c63c675835f1d24989a9d2d5
Faith Eyitope Adebayo <muyiwa@Faiths-MacBook-Pro.local>
2024-11-16 03:37:01 +0100

    slight issue with mongo getting solved

--- client/app/api/user/fetchUser/route.js
+++ client/app/api/user/fetchUser/route.js
@@ -1,33 +1,39 @@
+export const dynamic = "force-dynamic";
+
 import dbConnect from "@/Utils/Db";
 import User from "@/Models/User";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-    try {
-        await dbConnect();
+  try {
+    await dbConnect();
 
-        const { searchParams } = new URL(req.url);
-        const userId = searchParams.get("userId");
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
 
-        console.log("userId", userId);
+    console.log("userId", userId);
 
-        // Check if userId is provided
-        if (!userId) {
-            return NextResponse.json({ message: "User ID is required" }, { status: 400 });
-        }
-
-        const user = await User.findById(userId);
-        if (!user) {
-            return NextResponse.json({ message: "User not found" }, { status: 404 });
-        }
-        
-        return NextResponse.json({ user });
+    if (!userId) {
+      return NextResponse.json(
+        { message: "User ID is required" },
+        { status: 400 }
+      );
     }
-    catch (error) {
-        console.error("Error fetching user:", error);
-        return NextResponse.json(
-            { message: "An error occurred while fetching user", details: error.message },
-            { status: 500 }
-        );
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-};
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      {
+        message: "An error occurred while fetching user",
+        details: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
