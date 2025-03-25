fcfa6d79d66a7772a75e7573d04472d004439461
Dev-Faith <adebayofaith0@gmail.com>
2025-03-25 14:48:30 +0100

    disabling caching

--- client/app/api/bags/get/route.js
+++ client/app/api/bags/get/route.js
@@ -6,7 +6,15 @@ export async function GET() {
   await dbConnect();
   try {
     const product = await Product.find({});
-    return NextResponse.json(product);
+
+    // Create a new response with headers to disable caching
+    const response = NextResponse.json(product);
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+
+    return response;
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to get products", details: error.message },
